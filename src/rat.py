257d31fa8b66206a03cbec9eee34cb7087809eb1
jeremyanywhere <jeremycustomer@gmail.com>
2017-08-20 10:01:27 -0600

    Now using context managers for the threads

--- src/rat.py
+++ src/rat.py
@@ -14,37 +14,37 @@ class Rat(threading.Thread):
         maze[x][y] = "r"
 
     def run(self):
-        #wait or thread safe block..
-        print("Running a rat.run() with rat no. {}".format(Rat.rat_counter))
+        print(f"Creating rat at [{self.x},{self.y}]")
         Rat.rat_counter += 1
         rat_alive = True
         while rat_alive:
             time.sleep(2)
-            Rat.lock.acquire()
-            #check above, below
-            available_directions = 0
-            #check all directions
-            my_x = 0
-            my_y = 0
-            for p in [[1,0],[0,1],[-1,0],[0,-1]]:
-                if (self.x + p[0] in range(0,self.width) and
-                  self.y + p[1] in range(0,self.height) and
-                  self.maze[self.x+ p[0]][self.y+ p[1]] == '0'):
-                    available_directions +=1      # we can go in at least one direction.
-                    if available_directions == 1: # this rat will take the first direction
-                        self.maze[self.x + p[0]][self.y + p[1]] = 'r'
-                        my_x = self.x + p[0]
-                        my_y = self.y + p[1]
-                    if available_directions > 1:
-                        # there is more than one direction. Make a new Rat.
-                        rat = Rat(self.maze, self.x+ p[0], self.y+ p[1])
-                        rat.start()
-            self.x = my_x
-            self.y = my_y
-            if available_directions == 0:
-                #nowhere to go, we have to kill the rat.
-                rat_alive = False
-            Rat.lock.release()
+            #avoid race condition
+            with Rat.lock:
+                #check above, below
+                available_directions = 0
+                #check all directions
+                my_x = 0
+                my_y = 0
+                for p in [[1,0],[0,1],[-1,0],[0,-1]]:
+                    if (self.x + p[0] in range(0,self.width) and
+                      self.y + p[1] in range(0,self.height) and
+                      self.maze[self.x+ p[0]][self.y+ p[1]] == '0'):
+                        available_directions +=1      # we can go in at least one direction.
+                        if available_directions == 1: # this rat will take the first direction
+                            self.maze[self.x + p[0]][self.y + p[1]] = 'r'
+                            my_x = self.x + p[0]
+                            my_y = self.y + p[1]
+                        if available_directions > 1:
+                            # there is more than one direction. Make a new Rat.
+                            rat = Rat(self.maze, self.x+ p[0], self.y+ p[1])
+                            rat.start()
+                self.x = my_x
+                self.y = my_y
+                if available_directions == 0:
+                    #nowhere to go, we have to kill the rat.
+                    rat_alive = False
+
 
     def startMe(self):
         t1 = threading.Thread(target=self.run())
